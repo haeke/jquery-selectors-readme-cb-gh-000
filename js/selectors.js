076b4c8bd8aac31ec7327f1daeddf4d27be099bd
edwin rivera <waehake@gmail.com>
2018-01-19 15:25:44 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,12 +1,15 @@
 // declare your functions here...
 function paragraphSelector() {
   var p = $('body p');
+  return p;
 }
 
 function lastImageSelector() {
   var img = $('img:last');
+  return img;
 }
 
 function ninjaBabySelector() {
   var babyninja = $('#baby-ninja');
+  return babyninja;
 }
